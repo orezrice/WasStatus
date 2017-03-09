c4ac2cded4141ee192fff9305b96d6fd0fc3daac
ofeko <ofekot@gmail.com>
2017-03-09 19:03:31 +0200

    ListView updates

--- WhatStatus/app/src/main/java/com/whatstatus/MainActivity.java
+++ WhatStatus/app/src/main/java/com/whatstatus/MainActivity.java
@@ -2,17 +2,27 @@ package com.whatstatus;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.nfc.NfcAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
 import java.nio.ByteBuffer;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     private NfcAdapter mNfcAdapter;
+
+    private ListView inHouse;
+    private ListView outHouse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(com.whatstatus.R.layout.activity_main);
 
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
 
         if (mNfcAdapter == null) {
             // Stop here, we definitely need NFC
@@ -35,6 +46,36 @@ public class MainActivity extends AppCompatActivity {
         }
 
         handleIntent(getIntent());
+        loadList();
+    }
+
+    public String convertImageToSring(int imageId) {
+
+        Bitmap icon = BitmapFactory.decodeResource(this.getResources(), imageId);
+        return encodeToBase64(icon, Bitmap.CompressFormat.JPEG, 100);
+
+    }
+
+    public void loadList(){
+        inHouse = (ListView) findViewById(R.id.inhouselist);
+        outHouse = (ListView) findViewById(R.id.outhouselist);
+        final ListItem item = new ListItem("דונלד טראמפ",
+                convertImageToSring(R.drawable.exampleperson));
+
+        ListAdapter inHouseAdapter = new ListAdapter(this,
+                new LinkedList<ListItem>(){{add(item);}});
+        ListAdapter outHouseAdapter = new ListAdapter(this,
+                new LinkedList<ListItem>(){{add(item);}});
+
+        inHouse.setAdapter(inHouseAdapter);
+        outHouse.setAdapter(outHouseAdapter);
+    }
+
+    public String encodeToBase64(Bitmap image, Bitmap.CompressFormat compressFormat, int quality)
+    {
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        image.compress(compressFormat, quality, byteArrayOS);
+        return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
     }
 
     @Override
