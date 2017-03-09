f01f6497eedc309f8e06b46dbd96eee02ccf319f
ofeko <ofekot@gmail.com>
2017-03-09 16:07:25 +0200

    added list view of people

--- WhatStatus/app/src/main/java/com/whatstatus/MainActivity.java
+++ WhatStatus/app/src/main/java/com/whatstatus/MainActivity.java
@@ -1,21 +1,59 @@
 package com.whatstatus;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.util.LinkedList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView inHouse;
+    ListView outHouse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.whatstatus.R.layout.activity_main);
 
+        inHouse = (ListView) findViewById(R.id.inhouselist);
+        outHouse = (ListView) findViewById(R.id.outhouselist);
+        final ListItem item = new ListItem("דונלד טראמפ",
+                convertImageToSring(R.drawable.exampleperson));
+
+        ListAdapter inHouseAdapter = new ListAdapter(this,
+                new LinkedList<ListItem>(){{add(item);}});
+        ListAdapter outHouseAdapter = new ListAdapter(this,
+                new LinkedList<ListItem>(){{add(item);}});
+
+        inHouse.setAdapter(inHouseAdapter);
+        outHouse.setAdapter(outHouseAdapter);
+    }
+
+    public String convertImageToSring(int imageId) {
+
+        Bitmap icon = BitmapFactory.decodeResource(this.getResources(), imageId);
+        return encodeToBase64(icon, Bitmap.CompressFormat.JPEG, 100);
 
     }
+
+    public String encodeToBase64(Bitmap image, Bitmap.CompressFormat compressFormat, int quality)
+    {
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        image.compress(compressFormat, quality, byteArrayOS);
+        return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
