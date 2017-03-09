3e9a0e22ceb449256cadfd4f28c7816e5e07f6c2
ofeko <ofekot@gmail.com>
2017-03-09 18:57:52 +0200

    no message

---
+++ WhatStatus/app/src/main/java/com/whatstatus/NfcDisabledActivity.java
@@ -0,0 +1,13 @@
+package com.whatstatus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NfcDisabledActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_disabled);
+    }
+}
